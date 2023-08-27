476ad02966f95fce55143d22a245925340c5d94a
neuenmiller <kansapat4921@gmail.com>
2023-08-28 00:15:16 +0700

    Ping logic is added.

--- src/ping.py
+++ src/ping.py
@@ -1,5 +1,10 @@
 import subprocess 
 import time
 
+hostIP = "192.168.1.252"
+
 def pingPC ():
-    response = subprocess.run([])
+    response = subprocess.run(['ping', '-c', '1', hostIP], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return response.returncode == 0
+
+
